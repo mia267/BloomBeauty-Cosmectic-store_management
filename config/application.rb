b7438a09dc43f7909436d9bb3af74256f0004da3
mia267 <hoacthienkinh267@gmail.com>
2023-05-24 17:58:21 +0700

    Config for blocked hosting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module NguyenDucAnh0123456789SampleMacos
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "bloombeauty-app-production.up.railway.app"
+
   end
 end
